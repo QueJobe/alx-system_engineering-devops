8ff5fa417bfd8f3f2a0f5ff6966f20bd22573525
Blessing <bqsithole22@live.com>
2024-06-18 17:08:26 -0700

    Advanced API tasks

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """ 
+    Return number of subscribers in a subreddit 
+    """
+
+    header = {'User-Agent':  'Chrome/66.0.3359.139 Mobile Safari/537.36'}
+    url = "https://api.reddit.com/r/{}/about/".format(subreddit)
+    response = requests.get(url, headers=header)
+    if response.status_code == 200:
+        subs = response.json()["data"]["subscribers"]
+    else:
+        subs = 0
+    return subs
