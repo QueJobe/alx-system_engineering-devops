8a62b4177c7218ad804796882cc1d85caad11b9a
Blessing <bqsithole22@live.com>
2024-06-20 06:41:30 -0700

    correxting task 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,16 +5,11 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    """Return the number of subscribers in a subreddit """
+    """Return number of subscribers for subreddit"""
 
-    headers = {'User-Agent': 'my_user_agent/0.1 by your_username'}
+    header = {"User-Agent": "Jose98"}
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
-    
-    try:
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        if response.status_code == 200:
-            return response.json().get("data", {}).get("subscribers", 0)
-        else:
-            return 0
-    except requests.RequestException:
-        return 0
+    response = requests.get(url, headers=header)
+    if response.status_code == 200:
+        return response.json().get('data').get('subscribers')
+    return 0
